d8c319b0ee7b6e9bacca732a9ca1612288eb3d68
shivarag200701 <shivaraghav200701@gmail.com>
2025-06-19 19:01:49 -0500

    improved binary search

--- Binary_search/Binray_search.py
+++ Binary_search/Binray_search.py
@@ -2,26 +2,22 @@ class Solution(object):
     def search(self, nums, target):
         """
         :type nums: List[int]
+        :type nums: List[int]
         :type target: int
         :rtype: int
         """
         n = len(nums)
-        found = False
         i = 0
         j = n - 1
-        mid = (j - i) // 2
-        # print(mid)
-        while not found:
-            if target > nums[mid]:
+
+        while i <= j:
+            mid = i + (j - i) // 2  # Calculate mid correctly
+
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
                 i = mid + 1
-                mid = i + (j - i) // 2
-            elif target < nums[mid]:
-                j = mid - 1
-                mid = j - (j - i) // 2
             else:
-                found = True
+                j = mid - 1
 
-            if j < i:
-                return -1
-
-        return mid
+        return -1  # Target not found
